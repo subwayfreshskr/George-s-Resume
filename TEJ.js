e9bfe7435b672703a7a1278d92e9f92d28e8d183
subwayfreshskr <143437553+subwayfreshskr@users.noreply.github.com>
2024-07-31 20:53:46 +0800

    Update TEJ.js

--- TEJ.js
+++ TEJ.js
@@ -1,9 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('back-to-top');
+    const menuButton = document.getElementById('menu-button');
+    const nav = document.getElementById('main-nav');
+
+    function handleScroll() {
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        backToTopButton.classList.toggle('show', scrollTop > 200);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    if (backToTopButton) {
+        backToTopButton.addEventListener('click', function () {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+    }
+
+    if (menuButton && nav) {
+        menuButton.addEventListener('click', function () {
+            const isNavVisible = nav.classList.toggle('show');
+            menuButton.classList.toggle('change', isNavVisible);
+            nav.style.maxHeight = isNavVisible ? `${nav.scrollHeight}px` : '0';
+        });
+    }
+});
+
 let slideIndex = 1;
-let scrollCount = 0; // 初始化滾動計數器
-const scrollThreshold = 3; // 定義需要滾動幾次才換頁
+let scrollCount = 0;
+const scrollThreshold = 3;
 
 showSlides(slideIndex);
-
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
@@ -26,7 +52,6 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -36,17 +61,34 @@ function plusSlides(n) {
 }
 
 document.getElementById("browserWindow").addEventListener("wheel", function(event) {
-    event.preventDefault(); // 阻止默认滚动行为
+    event.preventDefault();
     scrollCount++;
 
     if (scrollCount >= scrollThreshold) {
         if (event.deltaY > 0) {
-            // 向下滚动
             plusSlides(1);
         } else {
-            // 向上滚动
             plusSlides(-1);
         }
-        scrollCount = 0; // 重置滾動計數器
+        scrollCount = 0;
+    }
+});
+
+
+let startX;
+document.getElementById("browserWindow").addEventListener("touchstart", function(event) {
+    startX = event.touches[0].clientX;
+});
+
+document.getElementById("browserWindow").addEventListener("touchend", function(event) {
+    let endX = event.changedTouches[0].clientX;
+    let difference = startX - endX;
+
+    if (Math.abs(difference) > 50) {
+        if (difference > 0) {
+            plusSlides(1);
+        } else {
+            plusSlides(-1);
+        }
     }
 });
